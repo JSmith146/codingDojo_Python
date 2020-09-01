d5755d3fc76fe1030bb1d505b549af074e7efc1c
Jeffrey Smith <mr.jeffrey.smithjr@gmail.com>
2020-09-01 06:43:06 -0400

    added method chaining to User

--- python/python_stack/_python/OOP/user.py
+++ python/python_stack/_python/OOP/user.py
@@ -6,43 +6,33 @@ class User:
         
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
     
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
     
     def display_user_balance(self):
         print(self.account_balance)
+        return self
     
     def transfer_money(self, other_user, amount):
         self.make_withdrawal(amount)
         other_user.make_deposit(amount)
         print(self.name,"transferred",amount,"to",other_user.name)
+        return self
 
 
 mike = User("Michael","Michael@gmail.com")
 john = User("John","John@gmail.com")
 susan = User("Susan", "susan@gmail.com")
-mike.make_deposit(100)
-mike.make_deposit(200)
-mike.make_deposit(50)
-mike.make_withdrawal(50)
-mike.display_user_balance()
 
-john.make_deposit(150)
-john.make_deposit(50)
-john.make_withdrawal(50)
-john.make_withdrawal(50)
-john.display_user_balance()
+mike.make_deposit(100).make_deposit(200).make_deposit(50).make_withdrawal(50).display_user_balance()
 
-susan.make_deposit(500)
-susan.make_withdrawal(25)
-susan.make_withdrawal(40)
-susan.make_withdrawal(35)
-susan.display_user_balance()
+john.make_deposit(150).make_deposit(50).make_withdrawal(50).make_withdrawal(50).display_user_balance()
 
-john.display_user_balance()
-susan.display_user_balance()
-john.transfer_money(susan,100)
-john.display_user_balance()
+susan.make_deposit(500).make_withdrawal(25).make_withdrawal(40).make_withdrawal(35).display_user_balance()
+
+john.transfer_money(susan,100).display_user_balance()
 susan.display_user_balance()
 
