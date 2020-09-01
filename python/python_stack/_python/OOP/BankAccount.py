de3ba61663ad7a4d436350fe2e4c90ad3ad9555d
Jeffrey Smith <mr.jeffrey.smithjr@gmail.com>
2020-09-01 07:03:48 -0400

    added BankAccount class

---
+++ python/python_stack/_python/OOP/BankAccount.py
@@ -0,0 +1,34 @@
+class BankAccount:
+    def __init__(self, int_rate=0.01, balance=0):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    
+    def withdraw(self, amount):
+        if (self.balance-amount < 0):
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+            return self
+        else:
+            self.balance -= amount
+            return self
+
+    def display_account_info(self):
+        print("Balance: $",self.balance)
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance *= (1+self.int_rate)
+            return self
+        else:
+            return self
+
+
+account_1 = BankAccount()
+account_2 = BankAccount()
+
+account_1.deposit(50).deposit(100).deposit(50).withdraw(25).yield_interest().display_account_info()
+account_2.deposit(200).deposit(300).withdraw(50).withdraw(25).withdraw(25).withdraw(25).yield_interest().display_account_info()
