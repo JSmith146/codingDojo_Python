b2dc819ff7642d03cbf9a0c2ae89624ad021b947
Jeffrey Smith <mr.jeffrey.smithjr@gmail.com>
2020-08-31 21:22:12 -0400

    added function intermediate

---
+++ python/python_stack/_python/python_fundamentals/functions_intermediate1.py
@@ -0,0 +1,17 @@
+import random
+def randInt(min=0, max=100):
+    if min > max:
+        temp = max
+        max = min
+        min = temp
+    elif max <0:
+        return null
+    num = round(random.random()*max + min)
+    return num
+
+
+print(randInt()) 		        # should print a random integer between 0 to 100
+print(randInt(max=50)) 	        # should print a random integer between 0 to 50
+print(randInt(min=50)) 	        # should print a random integer between 50 to 100
+print(randInt(min=50, max=500)) # should print a random integer between 50 and 500
+print(randInt(min=50, max=-10))
