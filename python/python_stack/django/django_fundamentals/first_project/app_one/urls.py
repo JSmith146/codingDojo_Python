003c0dcf11b9e9d679a0d147153001da4d07ecb6
Jeffrey Smith <mr.jeffrey.smithjr@gmail.com>
2020-09-14 09:34:46 -0400

    completed 'First Django Project'

--- python/python_stack/django/django_fundamentals/first_project/app_one/urls.py
+++ python/python_stack/django/django_fundamentals/first_project/app_one/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('new/', views.new),
+    path('create/',views.index),
+    path('<int:number>/', views.show),
+    path('<int:number>/edit/', views.edit),
+    path('<int:number>/delete/', views.destroy)
 ]
--- python/python_stack/django/django_fundamentals/first_project/app_one/views.py
+++ python/python_stack/django/django_fundamentals/first_project/app_one/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the same as @app.route('/')")
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return HttpResponse(index)
+
+def show(request, number):
+    return HttpResponse(f"placeholder to display blog number: {number}")
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog: {number}")
+
+def destroy(request, number):
+    return HttpResponse(index(request))
